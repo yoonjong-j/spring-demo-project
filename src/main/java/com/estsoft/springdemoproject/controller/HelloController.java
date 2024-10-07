8295dc0b98fe9dc06f1711b8190d84d568d53b18
icode1211 <icode1211@naver.com>
2024-10-07 16:21:39 +0900

    10/07 강의 중 코드 - 인터페이스 타입으로 의존성 주입(DI) 했을 때 구현체 지정하는 방법(@Qualifier, @Primary)

--- src/main/java/com/estsoft/springdemoproject/controller/HelloController.java
+++ src/main/java/com/estsoft/springdemoproject/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.estsoft.springdemoproject.controller;
 
+import com.estsoft.springdemoproject.interf.InterDependencyService;
 import com.estsoft.springdemoproject.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private HelloService service;
+    private final HelloService service;
+    private final InterDependencyService dependencyService;
 
-    public HelloController(HelloService service) {  // 생성자 주입(Dependency Injection) - DI 사용해서 spring에게 제어권 맡김
+    public HelloController(HelloService service, InterDependencyService dependencyService) {  // 생성자 주입(Dependency Injection) - DI 사용해서 spring에게 제어권 맡김
         this.service = service;
+        this.dependencyService = dependencyService;
     }
 
     @GetMapping("/hello")
@@ -19,6 +22,7 @@ public class HelloController {
         // 객체 직접 생성(객체 직접 제어 예시), 호출
 //        HelloService service = new HelloService();
 //        return service.printHello(param);
+        dependencyService.printMethod();
 
         return service.printHello(param);
     }
