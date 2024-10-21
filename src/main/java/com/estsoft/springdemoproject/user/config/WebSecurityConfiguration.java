206b0f095cdd4c02e02ebe7f5a20c791926740f5
icode1211 <icode1211@naver.com>
2024-10-21 16:44:54 +0900

    10/21 강의 중 코드(1) - 스프링 시큐리티 설정, userDetails, UserDetailService 구현체 추가

---
+++ src/main/java/com/estsoft/springdemoproject/user/config/WebSecurityConfiguration.java
@@ -0,0 +1,78 @@
+package com.estsoft.springdemoproject.user.config;
+
+import com.estsoft.springdemoproject.user.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+// 스프링 시큐리티 설정
+@Configuration
+public class WebSecurityConfiguration {
+
+    private final UserDetailService userDetailService;
+
+    public WebSecurityConfiguration(UserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    // 특정 요청은 스프링 시큐리티 설정을 타지 않도록 ignore 처리
+    @Bean
+    public WebSecurityCustomizer ignore() {
+        return webSecurity -> webSecurity.ignoring()
+                .requestMatchers(toH2Console())   // /h2-console
+                .requestMatchers("/static/**");
+    }
+
+    // 특정 요청에 대한 보안 구성 (주석 : 6.1 이전 ver. - 6.1 이후 부터는 람다 표현식으로 설정)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(  // 3) 인증, 인가 설정
+                    custom -> custom.requestMatchers("/login", "/signup", "/user").permitAll()
+                            .anyRequest().authenticated()
+                )
+//                .requestMatchers("/login", "/signup", "/user").permitAll()
+//                .anyRequest().authenticated()       // '위에서 정의한 url 이외에는 인증 필요'하다는 설정
+////                .requestMatchers("/api/external").hasRole("admin")  // 인가("ROLE_admin")
+
+
+                //4) 폼 기반 로그인 설정
+                .formLogin(custom -> custom.loginPage("/login")
+                        .defaultSuccessUrl("/articles"))
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/articles")     // 로그인 성공했을 경우 리디렉션 URL
+
+                // 5) 로그아웃 설정
+                .logout(custom -> custom.logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true))
+//                .logoutSuccessUrl("/login")
+//                .invalidateHttpSession(true)
+
+                // 6) csrf 비활성화
+                .csrf(custom -> custom.disable())
+//                .csrf().disable()
+                .build();
+    }
+
+    // 없어도 됨
+/*    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)  // 8) 사용자 정보 서비스 설정
+                .passwordEncoder(bCryptPasswordEncoder)
+                .build();
+    }*/
+
+
+    // 패스워드 암호화 방식 (BCryptPasswordEncoder) 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/estsoft/springdemoproject/user/domain/Users.java
@@ -0,0 +1,66 @@
+package com.estsoft.springdemoproject.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Entity
+public class Users implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Users() {
+    }
+
+    public Users(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {  // 인가 - 권한
+        return List.of(new SimpleGrantedAuthority("user"));   // 새싹, 주니어, 중니어, 시니어 , ...., 관리자
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // 계정의 만료 여부
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정의 잠김 여부
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 계정의 pw정보 만료 여부
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정 활성화
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
